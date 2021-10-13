9a39a4195bc0ebdecfc50fc2bc3e79a7e0c15060
c-yator <christian.yator@students.jkuat.ac.ke>
2021-10-13 10:34:03 +0300

    paddle movement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import Paddle from './paddle';
+import InputHandler from './input';
 
 let canvas = document.getElementById('gamescreen');
-if (canvas.getContext) {
-	/** @type {CanvasRenderingContext2D} */
-	let ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 
-	const GAME_WIDTH = 800;
-	const GAME_HEIGHT = 600;
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 
-	const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+paddle.draw(ctx);
+
+new InputHandler(paddle);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+	let deltaTime = timestamp - lastTime;
+	lastTime = timestamp;
+	ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+	paddle.update(deltaTime);
 	paddle.draw(ctx);
+	requestAnimationFrame(gameLoop);
 }
+
+gameLoop();
---
+++ src/input.js
@@ -0,0 +1,28 @@
+export default class InputHandler {
+	constructor(paddle) {
+		document.addEventListener('keydown', (event) => {
+			switch (event.key) {
+				case 'ArrowRight':
+					paddle.moveRight();
+					break;
+				case 'ArrowLeft':
+					paddle.moveLeft();
+					break;
+				default:
+					break;
+			}
+		});
+		document.addEventListener('keyup', (event) => {
+			switch (event.key) {
+				case 'ArrowRight':
+					if (paddle.speed > 0) paddle.stop();
+					break;
+				case 'ArrowLeft':
+					if (paddle.speed < 0) paddle.stop();
+					break;
+				default:
+					break;
+			}
+		});
+	}
+}
