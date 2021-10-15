0c0d375eb04d997a71b9d7017a31eb1fc8b40f99
c-yator <christian.yator@students.jkuat.ac.ke>
2021-10-15 08:29:31 +0300

    level switching

--- src/game.js
+++ src/game.js
@@ -1,13 +1,14 @@
 import InputHandler from './input';
 import Paddle from './paddle';
 import Ball from './ball';
-import { buildLevel, level1 } from './levels';
+import { buildLevel, level1, level2 } from './levels';
 
 const GAMESTATE = {
 	PAUSED: 0,
 	RUNNING: 1,
 	MENU: 2,
 	GAMEOVER: 3,
+	NEWLEVEL: 4,
 };
 
 export default class Game {
@@ -18,13 +19,21 @@ export default class Game {
 		this.paddle = new Paddle(this);
 		this.ball = new Ball(this);
 		this.gameObjects = [];
+		this.bricks = [];
 		this.lives = 3;
+		this.levels = [level1, level2];
+		this.currentLevel = 0;
 		new InputHandler(this.paddle, this);
 	}
 	start() {
-		if (this.gameState !== GAMESTATE.MENU) return;
-		let bricks = buildLevel(this, level1);
-		this.gameObjects = [this.paddle, this.ball, ...bricks];
+		if (
+			this.gameState !== GAMESTATE.MENU &&
+			this.gameState !== GAMESTATE.NEWLEVEL
+		)
+			return;
+		this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+		this.ball.reset();
+		this.gameObjects = [this.paddle, this.ball];
 		this.gameState = GAMESTATE.RUNNING;
 	}
 	update(deltaTime) {
@@ -35,13 +44,18 @@ export default class Game {
 			this.gameState === GAMESTATE.GAMEOVER
 		)
 			return;
-		this.gameObjects.forEach((object) => object.update(deltaTime));
-		this.gameObjects = this.gameObjects.filter(
-			(object) => !object.markedForDeletion
+		if (this.bricks.length === 0) {
+			this.currentLevel++;
+			this.gameState = GAMESTATE.NEWLEVEL;
+			this.start();
+		}
+		[...this.gameObjects, ...this.bricks].forEach((object) =>
+			object.update(deltaTime)
 		);
+		this.bricks = this.bricks.filter((brick) => !brick.markedForDeletion);
 	}
 	draw(ctx) {
-		this.gameObjects.forEach((object) => object.draw(ctx));
+		[...this.gameObjects, ...this.bricks].forEach((object) => object.draw(ctx));
 		if (this.gameState === GAMESTATE.PAUSED) {
 			ctx.rect(0, 0, this.gameWidth, this.gameHeight);
 			ctx.fillStyle = 'rgba(0,0,0,0.5)';
